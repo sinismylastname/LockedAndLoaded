1c3d0471e61d5768af26004212d8fddbbba9b289
Andy Sin <andygiabaosin@gmail.com>
2025-10-17 20:03:06 -0700

    working on menu transitions and the actual instantiating of the tp node

--- scenes/sidebar.gd
+++ scenes/sidebar.gd
@@ -4,22 +4,25 @@ var screen_width
 var mouse_x
 var menu_threshold = 900
 var opened
-var original_pos = Vector2(1200.0, 0.0)
-var opened_pos = Vector2(900.0, 0.0)
+var original_pos = Vector2(1200.0, 35.0)
+var opened_pos = Vector2(865.0, 35.0)
 
 @onready var sidebar_panel = $SidebarRoot/Panel
+@onready var tp_point_scene = preload("res://scenes/tp_point.tscn")
 
 func _ready() -> void:
 	screen_width = DisplayServer.screen_get_size().x
-	sidebar_panel.position = Vector2(1200.0, 0.0)
+	sidebar_panel.position = original_pos
 	
 func open_menu():
 	var tween = create_tween()
-	tween.tween_property(sidebar_panel, "position", opened_pos, 0.5)
+	tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE).tween_property(sidebar_panel, "position", opened_pos-Vector2(20, 0), 0.1)
+	tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE).tween_property(sidebar_panel, "position", opened_pos+Vector2(10, 0), 0.05)
+	tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE).tween_property(sidebar_panel, "position", opened_pos, 0.15)
 
 func close_menu():
 	var tween = create_tween()
-	tween.tween_property(sidebar_panel, "position", original_pos, 0.5)
+	tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE).tween_property(sidebar_panel, "position", original_pos, 0.3)
 	
 func _process(delta: float) -> void:
 	mouse_x = get_viewport().get_mouse_position().x
@@ -32,3 +35,10 @@ func _process(delta: float) -> void:
 		opened = false
 		close_menu()
 	#print(mouse_x)
+
+
+func _on_tppointspawn_pressed() -> void:
+	var new_tp_point = tp_point_scene.instantiate()
+	new_tp_point.global_position = Vector2(400, 400)
+	get_tree().current_scene.add_child(new_tp_point)
+	return
