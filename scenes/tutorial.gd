b8d60458502001d6ddb1bd34b5b7faba1e9eaf37
Andy Sin <andygiabaosin@gmail.com>
2025-11-06 01:05:54 -0800

    linux export 1

--- scenes/tutorial.gd
+++ scenes/tutorial.gd
@@ -3,6 +3,8 @@ extends Control
 var steps = 0
 @onready var label = $Label
 @onready var image = $Image
+@onready var img1 = preload("res://images/img1.png")
+@onready var img2 = preload("res://images/img2.png")
 
 func _ready():
 	pass
@@ -22,11 +24,22 @@ func _on_prev_pressed() -> void:
 func _process(delta: float) -> void:
 	if steps == 0:
 		label.text = "Welcome to Locked and Loaded!"
-		label.global_position = Vector2(258.0, 324.0)
+		label.global_position = Vector2(258.0, 300.0)
+		image.visible = false
 	if steps == 1:
 		label.text = "This tutorial will guide you through on how to play this game."
+		label.global_position = Vector2(258.0, 288.0)
+		image.visible = false
 	if steps == 2:
 		label.global_position = Vector2(284.0, 510.0)
 		label.text = "You are locked in place! (hence, the title)"
+		image.visible = true
+		image.texture = img1
+	if steps == 3:
+		label.text = "The only way you can move yourself is by rotation."
+		image.texture = img2
+	if steps == 4:
+		label.global_position = Vector2(284.0, 490.0)
+		label.text = "To defend yourself, you automatically shoot a bullet between short intervals."
 		
 	
